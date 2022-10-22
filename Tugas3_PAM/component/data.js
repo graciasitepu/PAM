fa9f854c372b265f038c71f5bc173ae01e028caf
gstp <104087436+graciasitepu@users.noreply.github.com>
2022-10-22 23:16:03 +0700

    Add files via upload

---
+++ Tugas3_PAM/component/data.js
@@ -0,0 +1,32 @@
+const DataPenerbangan = [
+    [
+      "Medan",
+      "Bandar Lampung",
+      "Jakarta",
+      "Maluku",
+      "Bali",
+    ],
+    [
+      "Bandar Lampung",
+      "Medan",
+      "Maluku",
+      "Jakarta",
+      "Bandar Lampung",
+    ],
+    [
+      "27 Maret 2023",
+      "27 Maret 2023",
+      "27 Maret 2023",
+      "27 Maret 2023",
+      "27 Maret 2023",
+    ],
+    [
+      "Elang",
+      "Tapis Air",
+      "Majapahit Air",
+      "Elang",
+      "Tapis Air",
+    ],
+  ];
+  
+  export default DataPenerbangan;
